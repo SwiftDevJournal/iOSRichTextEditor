abc743a8fc8693ab30f69c131a27fb96c476efa1
Mark Szymczyk <mark@meandmark.com>
2018-08-01 17:36:05 -0400

    Add code to scroll the text view properly when the onscreen keyboard appears.

--- iOSRichTextEditor/DocumentViewController.swift
+++ iOSRichTextEditor/DocumentViewController.swift
@@ -15,6 +15,7 @@ class DocumentViewController: UIViewController, UITextViewDelegate {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        registerForNotifications()
         
         // Access the document
         document?.open(completionHandler: { (success) in
@@ -27,6 +28,12 @@ class DocumentViewController: UIViewController, UITextViewDelegate {
         })
     }
     
+    func registerForNotifications() {
+        NotificationCenter.default.addObserver(self, selector:#selector(DocumentViewController.keyboardWasShown(notification:)), name:NSNotification.Name.UIKeyboardDidShow, object:nil)
+        NotificationCenter.default.addObserver(self, selector:#selector(DocumentViewController.keyboardWillBeHidden(notification:)), name:NSNotification.Name.UIKeyboardWillHide, object:nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(DocumentViewController.preferredContentSizeChanged(notification:)), name: NSNotification.Name.UIContentSizeCategoryDidChange, object: nil)
+    }
+    
     @IBAction func dismissDocumentViewController() {
         dismiss(animated: true) {
             self.document?.close(completionHandler: nil)
@@ -36,5 +43,26 @@ class DocumentViewController: UIViewController, UITextViewDelegate {
     func textViewDidChange(_ textView: UITextView) {
         document?.text = textView.attributedText
         document?.updateChangeCount(.done)
+    }
+    
+    
+    @objc func keyboardWasShown(notification: NSNotification) {
+        // Scroll the text view so the keyboard doesn't block what's being typed.
+        let info = notification.userInfo
+        if let keyboardRect = info?[UIKeyboardFrameBeginUserInfoKey] as? CGRect {
+            let keyboardSize = keyboardRect.size
+            textView.contentInset = UIEdgeInsetsMake(0, 0, keyboardSize.height, 0)
+            textView.scrollIndicatorInsets = textView.contentInset
+        }
+        
+    }
+    
+    @objc func keyboardWillBeHidden(notification: NSNotification) {
+        textView.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        textView.scrollIndicatorInsets = textView.contentInset
+    }
+    
+    @objc func preferredContentSizeChanged(notification: NSNotification) {
+        textView.font = UIFont.preferredFont(forTextStyle: UIFontTextStyle.body)
     }
 }
